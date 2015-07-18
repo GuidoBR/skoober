86427546f956b73ab6af3e119564613f5f6742d4
Guido <guidopercu@gmail.com>
2015-07-18 01:26:03 -0300

    Definindo os itens a serem extraidos

--- skoob/skoob/items.py
+++ skoob/skoob/items.py
@@ -9,6 +9,6 @@ import scrapy
 
 
 class SkoobItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()
+    author = scrapy.Field()
